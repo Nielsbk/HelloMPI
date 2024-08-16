2ae8ab6994729339e669b3aac7c75dbc491888c6
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2024-08-16 10:48:41 +0200

    Minor

--- snellius-2023/compile.sh
+++ snellius-2023/compile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#SBATCH --partition=rome
 #SBATCH --time=00:30:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=16
 #SBATCH --output=snellius-2023-compile.o
 #SBATCH --error=snellius-2023-compile.e
 set -e
