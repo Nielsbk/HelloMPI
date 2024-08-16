6a2b54366b671a208c71d98086ebf8dd2592c204
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2024-08-16 10:17:07 +0200

    More work for das6

--- das6/job-with-sysimage.sh
+++ das6/job-with-sysimage.sh
@@ -3,14 +3,14 @@
 #SBATCH --time=00:10:00
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=6
-#SBATCH --output=das6-job.o
-#SBATCH --error=das6-job.e
+#SBATCH --output=das6-job-with-sysimage.o
+#SBATCH --error=das6-job-with-sysimage.e
 
 source das6/modules.sh
 export OMP_NUM_THREADS=1
 export NUM_THREADS=1
 MPIFLAGS="--map-by node:span --rank-by core"
-JULIAFLAGS="-O3 --check-bounds=no"
+JULIAFLAGS="-O3 --check-bounds=no -J HelloMPI.so"
 mpiexec -np 24 $MPIFLAGS julia $JULIAFLAGS --project=. -e '
     using HelloMPI
     HelloMPI.greet()
